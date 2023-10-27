6c1bc42e310c1e09704e994425c8246b8afed86d
adansif <adan.yanesgonzalez@gmail.com>
2023-10-27 09:48:31 +0100

    feat(main): Extracted options from match-case

--- src/consult.py
+++ src/consult.py
@@ -5,7 +5,8 @@ def consult_all_cars(car_list):
     else:
         counter = 1
         for x in range(len(car_list)):
-            print((x+1), 'marca:', car_list[x]['brand'], 'modelo:', car_list[x]['model'])
+            print((x+1), 'marca:',
+                  car_list[x]['brand'], 'modelo:', car_list[x]['model'])
 
 
 def consult_especific_car(car_list, car_brand):
@@ -13,3 +14,15 @@ def consult_especific_car(car_list, car_brand):
         if (car_brand in x.values()):
             print(x)
     return car_list
+
+
+def consult_options(car_dictionary_list):
+    option = input(
+        'Desea consultar todos los coches o alguno en especifico? Ingrese \"Todo\" o \"Especifico\"\n')
+    if (option.lower() == 'todo'):
+        consult_all_cars(car_dictionary_list)
+    elif (option.lower() == 'especifico'):
+        car_brand = input('Ingrese la marca del coche que desea buscar\n')
+        consult_especific_car(car_dictionary_list, car_brand.capitalize())
+    else:
+        print('No ha elegido ninguna de las opciones anteriores')
--- src/remove.py
+++ src/remove.py
@@ -3,3 +3,14 @@ def remove_especific_car(car_list, car_brand):
         if(car_brand in x.values()):
             car_list.remove(x)
     return car_list
+
+def remove_options(car_dictionary_list):
+    option = input('Desea eliminar todos los coches o alguno en especifico? Ingrese \"Todo\" o \"Especifico\"\n')
+    if(option.lower() == 'todo'):
+        car_dictionary_list.clear()
+    elif(option.lower() == 'especifico'):
+        car_brand = input('Ingrese el modelo del coche que desea eliminar\n')
+        car_dictionary_list = remove_especific_car(car_dictionary_list, car_brand.capitalize())
+    else:
+        print('No ha elegido ninguna de las opciones anteriores')
+    return car_dictionary_list
